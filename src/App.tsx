09586fa87b8ee78acca0c7022af2769736cf847c
Simon Hessel <simon.hessel@kreios.lu>
2020-10-26 13:20:04 +0100

    added readme and githubpages support

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import { Login } from "./pages/Login";
 
 const App: React.FC = () => {
   return (
-    <Router basename="/">
+    <Router basename="/todo">
       <CssBaseline />
       <FirebaseProvider>
         <Switch>
