6bdc483a962836135a818ea8c8e1eaaf83d020e6
Simon Hessel <simon.hessel@kreios.lu>
2020-10-27 01:59:13 +0100

    added Login google authentication

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useContext } from "react";
+import {
+  Button,
+  Container,
+  createStyles,
+  makeStyles,
+  Theme,
+  Typography,
+} from "@material-ui/core";
+import firebase from "firebase";
+import { FirebaseContext } from "../context/FirebaseProvider";
 
 interface LoginProps {}
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+      height: "80vh",
+      alignItems: "center",
+      justifyContent: "space-between",
+      flexDirection: "column",
+    },
+  })
+);
+
 export const Login: React.FC<LoginProps> = ({}) => {
-  return <div>Login</div>;
+  const classes = useStyles();
+  const { auth } = useContext(FirebaseContext);
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+
+  return (
+    <Container>
+      <div className={classes.root}>
+        <Typography variant="h1">Welcome to Todo</Typography>
+        <Button onClick={signInWithGoogle} variant="contained" color="primary">
+          Sign in with Google
+        </Button>
+      </div>
+    </Container>
+  );
 };
